ff3136d5a5e3da3990598832f20e0a5a142c5a33
Remy Meier <remad@web.de>
2017-04-06 13:02:39 +0200

    pm2 dashboard works with cors test8

--- app/config/cors-whitelist.js
+++ app/config/cors-whitelist.js
@@ -2,5 +2,13 @@
  * Created by remy on 13/03/17.
  */
 module.exports = [
-  'http://100.127.254.98:8000'
+  'http://100.127.254.98:8000',
+  'http://localhost:3000',
+  'http://localhost:8000',
+  'http://localhost:8100',
+  'http://localhost:8101',
+  'http://100.127.254.3:3000',
+  'http://100.127.254.3:8000',
+  'http://100.127.254.3:8100',
+  'http://100.127.254.3:8101'
 ];
