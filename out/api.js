f179b88f93d2baaad24caf8783b195a617c44b29
Remy Meier <remad@web.de>
2017-04-21 12:52:34 +0200

    swagger ui not working

---
+++ out/api.js
@@ -0,0 +1,39 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "InventoryItemModel",
+        "app",
+        "connectSequelize",
+        "function"
+    ],
+    "modules": [
+        "cors",
+        "cors-whitelist",
+        "routes",
+        "routes-inventoryItemController"
+    ],
+    "allModules": [
+        {
+            "displayName": "cors",
+            "name": "cors",
+            "description": "Provides the cors with options"
+        },
+        {
+            "displayName": "cors-whitelist",
+            "name": "cors-whitelist",
+            "description": "Provides the cors with options"
+        },
+        {
+            "displayName": "routes",
+            "name": "routes",
+            "description": "Provides more features for the widget module..."
+        },
+        {
+            "displayName": "routes-inventoryItemController",
+            "name": "routes-inventoryItemController",
+            "description": "Provides more features for the widget module..."
+        }
+    ],
+    "elements": []
+} };
+});
